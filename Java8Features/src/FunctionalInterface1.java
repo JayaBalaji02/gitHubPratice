2e611d5b51243e688e382a726f6018c8c5ea53c8
Jaya.Balaji <jayabalaji057@gmail.com>
2025-06-30 15:38:25 +0530

    test 3

--- Java8Features/src/FunctionalInterface1.java
+++ Java8Features/src/FunctionalInterface1.java
@@ -2,7 +2,7 @@ public abstract class FunctionalInterface1 implements Message {
     public static void main(String[] args) {
         Message msg=()-> System.out.println("msg");
 
-        Message msg2=()-> System.out.println("msg -123");
+        Message msg2=()-> System.out.println("msg -2");
         msg.showMessages();
         msg2.showMessages();
     }
